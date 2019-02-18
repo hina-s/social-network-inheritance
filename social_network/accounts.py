fdad393405726e6ba66a2f3233945663f4c34cbd
hina-s <hina.11nov@gmail.com>
2019-02-18 03:23:53 +0000

    Passed tests

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,27 @@
+from datetime import datetime
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        
+        self.following = []
+        self.posts = []
+        
 
     def add_post(self, post):
-        pass
+        post.set_user(self)
+        self.posts.append(post)
 
     def get_timeline(self):
-        pass
+        timeline = []
+        for user in self.following:
+            timeline.extend(user.posts)
+            
+        return sorted(timeline, key=lambda p:p.timestamp, reverse=False)
+    # reverse is false as we sort in descending order of time
+            
 
     def follow(self, other):
-        pass
+        self.following.append(other)
